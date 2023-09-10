df2c9552c43869e5ed10caa58cb4fa918b507fbe
Imane Belaid <belaidimane@gmail.com>
2023-09-10 19:14:14 +0100

    Completed Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksElement = links.map((link) => {
+    // Modify the href attribute to include #
+    const href = `#${link}`
+    return <a href={href} key={link}>{link}</a>
+  })
+  return <nav>{/* display an <a> tag for each link here */} 
+    {linksElement}
+  </nav>;
 }
 
 export default NavBar;
